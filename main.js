dfe391ad86cb29fd5a177f2173d8a023d56f2bd7
Jacob Selbe <jalxselbe@gmail.com>
2018-12-22 17:48:40 -0600

    updated html/css/JS but not yet function

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 //watch form
+function watchForm() {
+    console.log('watchForm() ready');
+    $('#js-zipcodeForm').submit(e => {
+        e.preventDefault();
+        const userZipcode = $('#js-userZipcode').val();
+        console.log(userZipcode);
+        getMovies(userZipcode);
+    });
+}
 
 //get movies
+function getMovies(userZipcode) {
+    //fandango api key: 6b5nj8uq9qvj9xuqmbec8qmw
+    //EBU8HrBe8u
+    //endpoint:https://api.themoviedb.org/3/movie/550?api_key=771ac5f3dcc248eb6341b155a4ec98f4
+}
 
 //display movies
 
-//set page 
+
+//set page
+$(setPage => {
+    console.log('page set');
+    watchForm();
+}); 
