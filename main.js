35c69a368c9d26e36d1c243003f41b2e8b263ea9
Jacob Selbe <jalxselbe@gmail.com>
2018-12-24 14:55:59 -0600

    fixed reload on search, implemented error message

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const finalInput = [];
-const movieResults = [];
-
 function watchForm() {
     console.log('watchForm() ready');
     $('#js-searchForm').submit(e => {
@@ -15,6 +12,7 @@ function watchForm() {
 
 function refineInput(searchInput) {
     console.log('refineInput() executed');
+    const finalInput = [];
     let inputArray = searchInput
         .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g, '')
         .replace(/\s+/g, ' ')
@@ -26,45 +24,52 @@ function refineInput(searchInput) {
         }
     }
     console.log(finalInput);
-    getKeywordIds();
+    getKeywordIds(finalInput);
 }
 
-function getKeywordIds() {
+function getKeywordIds(finalInput) {
     console.log('getKeywordIds() executed');
+    const movieResults = [];
     for (let i = 0; i < finalInput.length; i++) {
         let keywordUrl = `https://api.themoviedb.org/3/search/keyword?api_key=771ac5f3dcc248eb6341b155a4ec98f4&query=${finalInput[i]}`;
         console.log(`keywordUrl${i}: ${keywordUrl}`);
         fetch(keywordUrl)
             .then(response => response.json())
-            .then(responseJson => getMovies(responseJson.results[0].id, i))
-            .catch(error => console.log(error));
+            .then(responseJson => getMovies(responseJson.results[0].id, i, movieResults))
+            .catch(error => {
+                console.log(error);
+                $('#js-results').html(`
+                    <p>Sorry, looks like we need a little more to go on than that. Please enter a little more description!</p>
+                `);
+            });
     }
 }
 
-function getMovies(id, i) {
+function getMovies(id, i, results) {
     let movieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=771ac5f3dcc248eb6341b155a4ec98f4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_keywords=${id}`;
-    console.log(`movieUrl${i}: ${movieUrl}`);
+    let r = i + 1;
+    console.log(`movieUrl${r}: ${movieUrl}`);
     fetch(movieUrl)
         .then(response => response.json())
-        .then(responseJson => filterMovies(responseJson.results))
+        .then(responseJson => filterMovies(responseJson.results, results))
         .catch(error => console.log(error));
 }
 
-function filterMovies(response) {
+function filterMovies(response, results) {
     for (let i = 0; i < response.length; i++) {
-        movieResults.push(response[i]);
+        results.push(response[i]);
     }
-    movieResults.sort(movie => (movie.popularity));
-    console.log(movieResults);
-    displayResults();
+    results.sort(movie => (movie.popularity));
+    console.log(results);
+    displayResults(results);
 }
 
-function displayResults() {
+function displayResults(results) {
     $('#js-results').html('');
-    for (let i = 0; i < movieResults.length; i++) {
+    for (let i = 0; i < results.length; i++) {
         let r = i + 1;
         $('#js-results').append(`
-            <p>${r}. ${movieResults[i].title}</p>
+            <p>${r}. ${results[i].title}</p>
         `);
     }
 }
